4c332969cf88dedc7067770d79d6bdf401cf3ded
Aous Abdulla <aa3033@york.ac.uk>
2023-04-20 17:04:05 +0100

    test and run on windows

--- core/src/main/com/mygdx/game/screens/NewMenuScreen.java
+++ core/src/main/com/mygdx/game/screens/NewMenuScreen.java
@@ -2,6 +2,7 @@ package com.mygdx.game.screens;
 
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class NewMenuScreen implements Screen {
     Texture playButton;
@@ -9,12 +10,17 @@ public class NewMenuScreen implements Screen {
     Texture quit;
     //Texture logo;
 
-public NewMenuScreen (){
+    SpriteBatch batch;
 
-    playButton = new Texture("MenuScreenPlay.png");
-    options = new Texture("MenuScreenOptions.png");
-    quit = new Texture("MenuScreenQuit.png");
-}
+
+    public NewMenuScreen (){
+
+
+        batch = new SpriteBatch();
+        playButton = new Texture("MenuScreenPlay.png");
+        options = new Texture("MenuScreenOptions.png");
+        quit = new Texture("MenuScreenQuit.png");
+    }
 
     @Override
     public void show() {
@@ -23,28 +29,23 @@ public NewMenuScreen (){
 
     @Override
     public void render(float delta) {
+        batch.begin();
+        batch.draw(playButton,0f ,0f);
+        batch.end();
 
     }
 
     @Override
-    public void resize(int width, int height) {
-
-    }
+    public void resize(int width, int height) {}
 
     @Override
-    public void pause() {
-
-    }
+    public void pause() {}
 
     @Override
-    public void resume() {
-
-    }
+    public void resume() {}
 
     @Override
-    public void hide() {
-
-    }
+    public void hide() {}
 
     @Override
     public void dispose() {
