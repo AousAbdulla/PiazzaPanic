f63257c870499691fabf1f499579dc2143aae2de
Aous Abdulla <aa3033@york.ac.uk>
2023-04-22 13:58:26 +0100

    test and run on windows

--- core/src/main/com/mygdx/game/screens/NewMenuScreen.java
+++ core/src/main/com/mygdx/game/screens/NewMenuScreen.java
@@ -3,45 +3,43 @@ package com.mygdx.game.screens;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class NewMenuScreen implements Screen {
     Texture playButton;
     Texture options;
     Texture quit;
+    //Texture logo;
 
     SpriteBatch batch;
 
-    ScreenViewport viewport;
+Viewport viewport;
+    public NewMenuScreen (){
 
-    public NewMenuScreen () {
+
         batch = new SpriteBatch();
         playButton = new Texture("textures/MenuScreenPlay.png");
         options = new Texture("textures/MenuScreenOptions.png");
         quit = new Texture("textures/MenuScreenQuit.png");
-
-        viewport = new ScreenViewport();
     }
 
     @Override
-    public void show() {}
+    public void show() {
+//test
+    }
 
     @Override
     public void render(float delta) {
-        viewport.apply();
-        batch.setProjectionMatrix(viewport.getCamera().combined);
-
         batch.begin();
-        batch.draw(playButton, 0f, 0f);
-        batch.draw(options, 50f, 0f);
-        batch.draw(quit, 100f, 0f);
+        batch.draw(playButton,0f ,0f);
+        batch.draw(options,50f ,0f);
+        batch.draw(quit,100f ,0f);
         batch.end();
-    }
 
-    @Override
-    public void resize(int width, int height) {
-        viewport.update(width, height);
     }
+//test
+    @Override
+    public void resize(int width, int height) {}
 
     @Override
     public void pause() {}
