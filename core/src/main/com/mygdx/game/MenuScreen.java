9036cea91a23b00a12588dbeaf2ba737e33ab6ea
AA <aa3033@york.ac.uk>
2023-04-30 11:57:58 +0100

    Menu buttons clickable (no screen redirection yet)

--- core/src/main/com/mygdx/game/MenuScreen.java
+++ core/src/main/com/mygdx/game/MenuScreen.java
@@ -9,7 +9,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-
+//
 public class MenuScreen implements Screen {
     Sprite playButton;
     Sprite options;
